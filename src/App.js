b532f887a4fb458b530f87b8300fdaa5daffaaad
Shehbaz Majeed <sshehbazmajeed@gmail.com>
2024-11-04 02:29:17 +0500

    Increament Derement React App Completed.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(15);
+  const minCount = 1;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} style={{'--logo-speed': count + 's'}} className="App-logo" alt="logo" />
+        <div className="card">
+          <div>
+            Counter: {count}
+          </div>
+          <div className='Button-container'>
+            <button className='App-button' onClick={() => setCount((count) => count + 1)}>Increament ++</button>
+            <button className='App-button' onClick={() => setCount((count) => count = minCount)}>Reset</button>
+            <button className='App-button' onClick={() => setCount((count) => (count > minCount ? count - 1 : minCount))} disabled={count === minCount}>Decreament --</button>
+          </div>
+          <div className='infoText'>
+            We are using the calculated value to increase/decrease the speed of our logo rotation. So, the minimum number of our counter is 1.
+          </div>
+        </div>
       </header>
     </div>
   );
